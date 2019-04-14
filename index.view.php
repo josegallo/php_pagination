34956514768fd2795b310542879b6b172f5340b7
josegallo <josegallo@gmail.com>
2019-04-14 16:48:44 +0200

    Basic View

---
+++ index.view.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Pagination</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link href='https://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+	<link rel = "stylesheet" href = "style.css">
+</head>
+<body>
+	<div class = "container">
+        <h1>Articles</h1>
+        <section class = 'articles'>
+            <ul>
+                <li> Lorem ipsum, dolor sit amet consectetur adipisicing elit.  </li>
+                <li> Lorem ipsum, dolor sit amet consectetur adipisicing elit.  </li>
+                <li> Lorem ipsum, dolor sit amet consectetur adipisicing elit.  </li>
+                <li> Lorem ipsum, dolor sit amet consectetur adipisicing elit.  </li>
+            </ul>
+        </section>
+        <section class = 'pagination'>
+            <ul>
+                <!-- <li class = 'disabled'> <a href ='#'>&laquo;</a></li> -->
+                <li class = 'disabled'>&laquo;</a></li>
+                <li class = 'active'> <a href="#">1</a> </li>
+                <li> <a href="#">2</a> </li>
+                <li> <a href="#">3</a> </li>
+                <li> <a href="#">4</a> </li>
+                <li> <a href="#">&raquo;</a></li>
+            </ul>
+        </section>
+	</div>
+
+
+</body>
+</html>
+
